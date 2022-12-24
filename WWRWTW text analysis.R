8964010dbfbb048d668875935d8260d4d6609779
Jenesequoi <46922974+Jenesequoi@users.noreply.github.com>
2022-12-24 12:48:51 +0300

    Add files via upload

--- WWRWTW text analysis.R
+++ WWRWTW text analysis.R
@@ -78,6 +78,74 @@ bigrams_filtered<-clean_bigrams%>%
   filter(!word1%in%stop_words$word)%>%
   filter(!word2%in%stop_words$word)
 
+#sort bigrams
 bigrams_count<-bigrams_filtered%>%
-  count(word1,word2,sort = TRUE)
-bigrams_count
+  count(word1,word2, sort = TRUE)
+
+
+unite_bigrams<-bigrams_filtered%>%
+  unite(bigram,word1,word2,sep = " ")%>%
+  count(bigram)%>%
+  arrange(desc(n))
+
+
+# bigram_tf_idf<-unite_bigrams%>%
+#   count(document, bigram)%>%
+#   bind_tf_idf(bigram,document,n)%>%
+#   arrange(desc(tf_idf))
+remove(bigram_tf_idf)
+
+#visualize bigrams
+unite_bigrams%>%
+  top_n(15)%>%
+  mutate(bigram = reorder(bigram, n))%>%
+  ggplot(aes(bigram,n,fill = "purple")) +
+  geom_col(show.legend = FALSE) +
+  ylab("frequency") +
+  coord_flip()
+
+#visualize using arrows
+library(influential)
+library(ggraph)
+visualize_bigrams<- function(bigrams) {
+  set.seed(2016)
+  a<-grid::arrow(type="closed",length=unit(.15,"inches"))
+  bigrams %>%
+    graph_from_data_frame() %>%
+    ggraph(layout="fr") +
+    geom_edge_link(aes(edge_alpha=n),show.legend=FALSE, arrow=a, end_cap=circle(.05,'inches')) +
+    geom_node_point(color="lightblue",size = 4) +
+    geom_node_text(aes(label=name),vjust=1,hjust=1) +
+    theme_void()
+}
+count_bigrams<- function(dataset) { dataset %>%
+    unnest_tokens(bigram, text, token="ngrams", n=2) %>%
+    separate(bigram, c("word1","word2"),sep=" ") %>%
+    filter(!word1 %in% stop_words$word)%>%
+    filter(!word2 %in% stop_words$word)%>% count(word1,word2,sort=TRUE)
+}
+
+bigrams<- text_df%>%
+  unnest%>%
+  count_bigrams()
+
+library(stringr)
+
+bigrams %>% filter(n>20,
+                   !str_detect(word1, "\\d"),
+                   !str_detect(word2, "\\d")) %>% visualize_bigrams()
+
+#sentiment analysis
+strategic_sentiments<-unite_bigrams%>%
+  inner_join(get_sentiments("bing"),by=c(word="word1"))
+
+strategic_sentiments %>%
+  count(sentiment,word,wt=count)%>%
+  ungroup()%>%
+  filter(n>=10)%>%
+  mutate(n=ifelse(sentiment=="negative", -n, n))%>%
+  mutate(word=reorder(word,n))%>%
+  ggplot(aes(word,n,fill=sentiment)) +
+  geom_bar(stat = "identity") +
+  ylab("Contribution to sentiment") +
+  coord_flip()
